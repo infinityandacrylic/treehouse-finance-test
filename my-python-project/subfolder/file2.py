a196b6bbda3c6c393952135777266b5bdb280bc2
infinityandacrylic <85436757+infinityandacrylic@users.noreply.github.com>
2021-08-25 16:05:06 +0800

    Q5 test files

---
+++ my-python-project/subfolder/file2.py
@@ -0,0 +1,12 @@
+print("test1")
+print("test2")
+# comment line 1
+# comment line 2
+def functionB():
+	print("hello world")
+print("test1")
+print("test2")
+# comment line 1
+# comment line 2
+def functionC():
+	print("hello world")
---
+++ my-python-project/subfolder2/subsubfolder/file3.py
@@ -0,0 +1,6 @@
+print("test1")
+print("test2")
+# comment line 1
+# comment line 2
+def functionD():
+	print("hello world")
